2d15db9129f9d20e64d93e6a0c991a2485937b4f
Henry Dashwood <henry.dashwood@curationcorp.com>
2020-09-06 14:31:34 +0100

    initial commit

---
+++ project/app/config.py
@@ -0,0 +1,15 @@
+from fastapi.logger import logger
+import os
+from functools import lru_cache
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    environment: str = os.getenv("ENVIRONMENT", "dev")
+    testing: bool = os.getenv("TESTING", 0)
+
+
+@lru_cache()
+def get_settings() -> BaseSettings:
+    logger.warning("Loading config settings from the environment...")
+    return Settings()
